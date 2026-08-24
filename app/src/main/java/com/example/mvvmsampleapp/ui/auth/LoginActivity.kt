16ae111ae3099f2f908a4c73221f805a49d65508
Barta Balazs <barta.balazs00@gmail.com>
2026-08-24 18:14:08 +0200

    Data Binding

--- app/src/main/java/com/example/mvvmsampleapp/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/example/mvvmsampleapp/ui/auth/LoginActivity.kt
@@ -5,12 +5,35 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.mvvmsampleapp.R
+import com.example.mvvmsampleapp.databinding.ActivityLoginBinding
+import com.example.mvvmsampleapp.util.toast
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), AuthListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        val binding: ActivityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+        val viewModel = ViewModelProvider(this)[AuthViewModel::class.java]
+
+        binding.viewmodel = viewModel
+
+        viewModel.authListener = this
+    }
+
+    override fun onStarted() {
+        toast("Login Started")
+    }
+
+    override fun onSuccess() {
+        toast("Login Success")
+    }
+
+    override fun onFailure(message: String) {
+        toast(message)
     }
 }
