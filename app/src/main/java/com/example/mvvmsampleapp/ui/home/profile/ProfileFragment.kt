89e24440a7bc132d66005a26387dd4bdf7946e56
Barta Balazs <barta.balazs00@gmail.com>
2026-09-04 18:19:53 +0200

    Data binding in Fragment

--- app/src/main/java/com/example/mvvmsampleapp/ui/home/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/mvvmsampleapp/ui/home/profile/ProfileFragment.kt
@@ -1,31 +1,33 @@
 package com.example.mvvmsampleapp.ui.home.profile
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.mvvmsampleapp.R
+import com.example.mvvmsampleapp.databinding.ProfileFragmentBinding
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.kodein
+import org.kodein.di.generic.instance
 
-class ProfileFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = ProfileFragment()
-    }
-
-    private val viewModel: ProfileViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
+class ProfileFragment : Fragment(), KodeinAware {
+    override val kodein: Kodein by kodein()
+    private val factory: ProfileViewModelFactory by instance()
+    private lateinit var viewModel: ProfileViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.profile_fragment, container, false)
+        val binding: ProfileFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.profile_fragment, container, false)
+        viewModel = ViewModelProvider(this, factory)[ProfileViewModel::class.java]
+        binding.viewmodel = viewModel
+        binding.lifecycleOwner= this
+        return binding.root
     }
+
 }
