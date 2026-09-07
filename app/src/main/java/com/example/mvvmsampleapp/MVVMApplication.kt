fb5b58895f86e68bf2cfdeaa437226fb17a64b02
Barta Balazs <barta.balazs00@gmail.com>
2026-09-07 17:53:24 +0200

    SharedPreferences, only cann api if needed

--- app/src/main/java/com/example/mvvmsampleapp/MVVMApplication.kt
+++ app/src/main/java/com/example/mvvmsampleapp/MVVMApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.example.mvvmsampleapp.data.db.AppDatabase
 import com.example.mvvmsampleapp.data.network.MyApi
 import com.example.mvvmsampleapp.data.network.NetworkConnectionInterceptor
+import com.example.mvvmsampleapp.data.preferences.PreferenceProvider
 import com.example.mvvmsampleapp.data.repositories.QuotesRepository
 import com.example.mvvmsampleapp.data.repositories.UserRepository
 import com.example.mvvmsampleapp.ui.auth.AuthViewModelFactory
@@ -24,7 +25,8 @@ class MVVMApplication : Application(), KodeinAware {
         bind() from singleton { MyApi(instance()) }
         bind() from singleton { AppDatabase(instance()) }
         bind() from singleton { UserRepository(instance(), instance()) }
-        bind() from singleton { QuotesRepository(instance(), instance()) }
+        bind() from singleton { PreferenceProvider(instance())}
+        bind() from singleton { QuotesRepository(instance(), instance(), instance()) }
         bind() from provider { AuthViewModelFactory(instance()) }
         bind() from provider { ProfileViewModelFactory(instance()) }
         bind() from provider { QuotesViewModelFactory(instance()) }
--- app/src/main/java/com/example/mvvmsampleapp/data/repositories/QuotesRepository.kt
+++ app/src/main/java/com/example/mvvmsampleapp/data/repositories/QuotesRepository.kt
@@ -6,13 +6,18 @@ import com.example.mvvmsampleapp.data.db.AppDatabase
 import com.example.mvvmsampleapp.data.db.entities.Quote
 import com.example.mvvmsampleapp.data.network.MyApi
 import com.example.mvvmsampleapp.data.network.SafeApiRequest
+import com.example.mvvmsampleapp.data.preferences.PreferenceProvider
 import com.example.mvvmsampleapp.util.Coroutines
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
 
+private val MIMINUM_INTERVAL = 6
 class QuotesRepository(
     private val api: MyApi,
-    private val db: AppDatabase
+    private val db: AppDatabase,
+    private val prefs: PreferenceProvider
 ): SafeApiRequest(){
     private val quotes = MutableLiveData<List<Quote>>()
 
@@ -29,18 +34,20 @@ class QuotesRepository(
     }
 
     private suspend fun fetchQuotes(){
-        if(isFetchNeeded()){
+        val lastSavedAt = prefs.getLastSavedAt()
+        if(lastSavedAt == null || isFetchNeeded(LocalDateTime.parse(lastSavedAt))){
             val response= apiRequest { api.getQuotes() }
             quotes.postValue(response.quotes)
         }
     }
 
-    private fun isFetchNeeded(): Boolean {
-        return true
+    private fun isFetchNeeded(SavedAt : LocalDateTime): Boolean {
+        return ChronoUnit.HOURS.between(SavedAt, LocalDateTime.now()) > MIMINUM_INTERVAL
     }
 
     private fun saveQuotes(quotes : List<Quote>){
         Coroutines.io {
+            prefs.savelastSavedAt(LocalDateTime.now().toString())
             db.getQuoteDao().saveAllQuotes(quotes)
         }
     }
